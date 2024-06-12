2e5a39e6c38f7218124c36a8d42725149b682654
KinGelaim <89123232156@mail.ru>
2024-06-12 19:31:02 +0500

    Added a clock

---
+++ MyLibrarySpace/MyLibrarySpace/Interop/Clock.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLibrarySpace.Interop;
+
+/// <summary>
+/// Сосредоточение доступа ко времени в проекте
+/// </summary>
+public sealed class Clock : IClock
+{
+    private readonly DateTime? _fixedTime;
+
+    /// <inheritdoc/>
+    public DateTime Now => _fixedTime ?? DateTime.Now;
+
+    /// <inheritdoc/>
+    public DateTime Today => _fixedTime?.Date ?? DateTime.Today;
+
+    public Clock(DateTime? dateTime = null)
+    {
+        _fixedTime = dateTime;
+    }
+}
---
+++ MyLibrarySpace/MyLibrarySpace/Interop/IClock.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLibrarySpace.Interop;
+
+/// <summary>
+/// Интерфейс для сосредоточения доступа ко времени в проекте
+/// </summary>
+public interface IClock
+{
+    /// <summary>
+    /// Сейчас
+    /// </summary>
+    DateTime Now { get; }
+
+    /// <summary>
+    /// Сегодня
+    /// </summary>
+    DateTime Today { get; }
+}
