6a0a251f10f6309760262fe928b31c5b518075ba
KinGelaim <89123232156@mail.ru>
2024-06-12 19:30:41 +0500

    Added an extension for datetime

---
+++ MyLibrarySpace/MyLibrarySpace/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLibrarySpace.Extensions;
+
+/// <summary>
+/// Расширения для представления дат и времени
+/// </summary>
+public static class DateTimeExtensions
+{
+    /// <summary>
+    /// Вывод даты-времени в формате 2023-01-01 13:23:10
+    /// </summary>
+    /// <param name="dateTime">Исходные дата-время</param>
+    public static string ToDateAndTime(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+}
