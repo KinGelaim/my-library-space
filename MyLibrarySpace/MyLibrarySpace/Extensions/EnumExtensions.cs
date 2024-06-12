30d5abe833bf47144a43f4bff95dc737043e4119
KinGelaim <89123232156@mail.ru>
2024-06-12 19:30:35 +0500

    Added an extension for enum

---
+++ MyLibrarySpace/MyLibrarySpace/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLibrarySpace.Extensions;
+
+/// <summary>
+/// Расширения для перечислений
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// Получение атрибута описания <see cref="System.ComponentModel.DescriptionAttribute"/>
+    /// </summary>
+    /// <param name="enumValue">Значение перечисления</param>
+    /// <returns>Описание или null, если значение не было найдено</returns>
+    public static string? ToDescription(this Enum enumValue)
+    {
+        var type = enumValue.GetType();
+        var memInfo = type.GetMember(enumValue.ToString());
+        var attributes = memInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute), false);
+        return (attributes.Length > 0) ? attributes[0].ToString() : null;
+    }
+}
