ecb76d2e5b7df40141d82bc270dbba72031e9525
KinGelaim <89123232156@mail.ru>
2024-06-12 19:30:26 +0500

    Added an extension for swagger

---
+++ MyLibrarySpace/MyLibrarySpace/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+
+namespace MyLibrarySpace.Extensions;
+
+/// <summary>
+/// Расширения для настроек Swagger
+/// </summary>
+public static class SwaggerGenOptionsExtensions
+{
+    /// <summary>
+    /// Маппер для <see cref="DateOnly"/> в строку
+    /// </summary>
+    public static void UseDateOnlyStringConverters(this SwaggerGenOptions options)
+    {
+        options.MapType<DateOnly>(() => new OpenApiSchema
+        {
+            Type = "string",
+            Format = "date"
+        });
+    }
+}
